5be327e6acaa8b71d10360a36f89d3d20a478439
Russell Malenchek <malenchek@gmail.com>
2016-12-14 17:20:43 -0500

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# Rails.application.routes.draw do
+#   post 'contact', to: 'contacts#process_form'
+#   root to: 'visitors#new'
+# end
+
+
 Rails.application.routes.draw do
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
 end
+
