2de51a71e06f4219eee87415081d2c64f7f07292
Russell Malenchek <malenchek@gmail.com>
2016-12-15 16:14:25 -0500

    mailing list

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
-# Rails.application.routes.draw do
-#   post 'contact', to: 'contacts#process_form'
-#   root to: 'visitors#new'
-# end
-
-
 Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
 end
-
